7b2d4e8f4fe45311c6cdb1aea99e263920f6fb03
let-sampaio <leticia.sampaio.souza08@aluno.ifce.edu.br>
2024-05-11 23:02:40 -0300

    Atualizações ✨

--- tela.js
+++ tela.js
@@ -1,15 +1,21 @@
-       // Seleciona os botões
-       const btnProjetos = document.getElementById('btn-projetos');
-       const btnSkills = document.getElementById('btn-skills');
+const express = require('express');
+const app = express();
+const path = require('path');
 
-       // Adiciona um ouvinte de evento de clique ao botão de projetos
-       btnProjetos.addEventListener('click', function() {
-           // Redireciona para a página de projetos
-           window.location.href = 'projetos.html'; // Substitua 'pagina_projetos.html' pelo caminho da sua página de projetos
-       });
+const port = 3000;
 
-       // Adiciona um ouvinte de evento de clique ao botão de contato
-       btnSkills.addEventListener('click', function() {
-           // Redireciona para a página de contato
-           window.location.href = 'skills.html'; // Substitua 'pagina_contato.html' pelo caminho da sua página de contato
-       });
+app.use(express.static(path.join(__dirname, '')));
+
+
+app.get('', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.get('/projetos',(req,res) =>{
+    res.sendFile(path.resolve(__dirname,'projetos.html'));
+});
+
+app.get('/skills',(req,res) =>{
+    res.sendFile(path.resolve(__dirname,'skills.html'));
+});
+app.listen(port, () => console.log(`listening on port ${port}!`));
